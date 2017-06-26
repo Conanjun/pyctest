476876b30db8d71d3e6ef133183c9dc365bbc5cd
Conanjun <1526840124@qq.com>
2017-06-26 15:03:26 +0800

    remove py

--- exp_package/exp.py
+++
@@ -1,8 +0,0 @@
-import exp_mod
-
-def exploit():
-    exp_mod.exploit()
-    print("exp:exploiting... by pyc package/exp.pyc")
-
-if __name__=="__main__":
-    exploit()
--- exp_package/exp_mod.py
+++
@@ -1,2 +0,0 @@
-def exploit():
-    print("EXP_MOD:exploiting ... by pyc package/exp_mod.pyc")
